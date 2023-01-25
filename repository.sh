c97b32fed9f8ed0fbab6a8fe2a54427641fdfb3f
Leopold Palomo-Avellaneda <leo@alaxarxa.net>
2023-01-25 19:29:20 +0100

    Fix repo url

--- repository.sh
+++ repository.sh
@@ -6,11 +6,10 @@ set -ex
 vcs export src --exact-with-tags > /home/runner/apt_repo/sources.repos
 
 cd /home/runner/apt_repo
-#apt-ftparchive packages . > Packages
-#apt-ftparchive release . > Release
 
 REPOSITORY="$(printf "%s" "$GITHUB_REPOSITORY" | tr / _)"
 echo '```bash' > README.md
-echo "echo \"deb [trusted=yes] https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$DEB_DISTRO-$ROS_DISTRO/ ./\" | sudo tee /etc/apt/sources.list.d/$REPOSITORY.list" >> README.md
+echo "echo \"deb [trusted=yes,arch=amd64] https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$DEB_DISTRO-$ROS_DISTRO $DEB_DISTRO main" | sudo tee /etc/apt/sources.list.d/$REPOSITORY.list" >> README.md
 echo "echo \"yaml https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$DEB_DISTRO-$ROS_DISTRO/local.yaml $ROS_DISTRO\" | sudo tee /etc/ros/rosdep/sources.list.d/1-$REPOSITORY.list" >> README.md
 echo '```' >> README.md
+deb [trusted=yes] https://raw.githubusercontent.com/lepalom/ros-one-packages/bullseye-debian bullseye main
