951515a39bbbcb6947a293b4a0a5360bd2081bb6
Leopold Palomo-Avellaneda <leo@alaxarxa.net>
2023-01-25 19:43:59 +0100

    Fix repo url with gitlab pages

--- repository.sh
+++ repository.sh
@@ -9,7 +9,6 @@ cd /home/runner/apt_repo
 
 REPOSITORY="$(printf "%s" "$GITHUB_REPOSITORY" | tr / _)"
 echo '```bash' > README.md
-echo "echo \"deb [trusted=yes,arch=amd64] https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$DEB_DISTRO-$ROS_DISTRO $DEB_DISTRO main" | sudo tee /etc/apt/sources.list.d/$REPOSITORY.list" >> README.md
+echo "echo \"deb [trusted=yes arch=amd64] https://lepalom.github.io/$GITHUB_REPOSITORY $DEB_DISTRO  main | sudo tee /etc/apt/sources.list.d/$REPOSITORY.list" >> README.md
 echo "echo \"yaml https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$DEB_DISTRO-$ROS_DISTRO/local.yaml $ROS_DISTRO\" | sudo tee /etc/ros/rosdep/sources.list.d/1-$REPOSITORY.list" >> README.md
 echo '```' >> README.md
-deb [trusted=yes] https://raw.githubusercontent.com/lepalom/ros-one-packages/bullseye-debian bullseye main
